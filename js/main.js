661dc7fedd89711c94cfe5a2833b61e82694c425
Kyron Wilson <inf6mous9@gmail.com>
2025-12-04 17:55:53 -0600

    js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 // JS scripts placed here
+
+const slideContainer = document.getElementById("slideContainer");
+const slides = document.querySelectorAll(".slides img");
+let index = 0;
+
+function showSlide(i) {
+  index = (i + slides.length) % slides.length;
+  slideContainer.style.transform = `translateX(${-index * 600}px)`;
+}
+
+document.getElementById("next").addEventListener("click", () => {
+  showSlide(index + 1);
+});
+
+document.getElementById("prev").addEventListener("click", () => {
+  showSlide(index - 1);
+});
+
+// Auto slide every 3 seconds
+setInterval(() => {
+  showSlide(index + 1);
+}, 3000);
